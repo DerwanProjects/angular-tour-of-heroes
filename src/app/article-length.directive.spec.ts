e657c65a90e3f1d159080c82ce45e935f0638a8d
wronait <piotrdeja.pl@gmail.com>
2022-04-13 23:22:34 +0200

    zadanie z sesji Angular 9 kwietnia 2022r.

---
+++ src/app/article-length.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ArticleLengthDirective } from './article-length.directive';
+
+describe('ArticleLengthDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ArticleLengthDirective();
+    expect(directive).toBeTruthy();
+  });
+});
