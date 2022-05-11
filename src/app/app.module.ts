aa9f7bcca54bbea4a3586ce7bff6719e97808969
wronait <piotrdeja.pl@gmail.com>
2022-05-11 21:43:08 +0200

    7maja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,12 @@ import { ClickService } from './services/click.service';
 import { TableComponent } from './table/table.component';
 import { Highlight2Directive } from './highlight-2.directive';
 import { GetTableRowIdDirective } from './get-table-row-id.directive';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { AppRouterModule } from './app-router.module';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+
 
 @NgModule({
   declarations: [
@@ -35,9 +41,14 @@ import { GetTableRowIdDirective } from './get-table-row-id.directive';
     TableComponent,
     Highlight2Directive,
     GetTableRowIdDirective,
+    Page1Component,
+    Page2Component,
+    ChildAComponent,
+    ChildBComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule
   ],
   providers: [ClickService],
   bootstrap: [AppComponent]
